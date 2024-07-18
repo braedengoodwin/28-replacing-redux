7520dead9e2c73aea3ad67c86b0277e9be3354f6
braedengoodwin <braedenagoodwin@hotmail.com>
2024-07-18 18:41:18 -0500

    finished this section, very confusing

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+import configureStore from './hooks-store/products-store';
 
-const store = createStore(rootReducer);
+configureStore()
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
